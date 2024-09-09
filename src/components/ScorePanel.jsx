72b64590f0697164f4f29484f38b4205c4133bcd
Vonglory176 <SkylerGConley@gmail.com>
2024-09-08 19:55:24 -0700

    Made some changes to hidden classes and header types

--- src/components/ScorePanel.jsx
+++ src/components/ScorePanel.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ScorePanel = () => {
   return (
-    <div id="scorePanel" className="hidden md:block rounded bg-purple-800 bg-opacity-50">            
-        <h2 className='text-center text-2xl'>Score</h2>
+    <div id="scorePanel" className="rounded bg-purple-800 bg-opacity-50">            
+        <h3 className='text-center text-2xl'>Score</h3>
         <div id="scoreContainer" className=""></div>
     </div>
   )
