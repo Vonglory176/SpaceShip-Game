bc1229cf1368063ef8820615f5a8048c94ef23e8
Vonglory176 <SkylerGConley@gmail.com>
2024-09-13 14:44:21 -0700

    Updated to use Context

--- src/components/CanvasComponent.jsx
+++ src/components/CanvasComponent.jsx
@@ -1,16 +1,26 @@
 import React, { useRef, useEffect } from 'react'
+import { useSiteContext } from '../context/siteContext'
+import { useAssetsContext } from '../context/assetContext'
 
-const CanvasComponent = ({ width, height, startGame, gameState, getScore, assets, gameDifficulty }) => {
+const CanvasComponent = ({ width, height, startGame, getScore }) => {
   const canvasRef = useRef(null)
+  const { gameState, gameDifficulty } = useSiteContext()
+  const { assets } = useAssetsContext()
 
+  const difficultyMap = { // Determines asteroid speed using this as a multiplier
+    "easy": 1,
+    "medium": 2,
+    "hard" : 3
+  }
+  
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
-
+    
     if (gameState === 'playing' && assets) {
-      startGame(canvas, ctx, assets, gameDifficulty, getScore)
+      startGame(canvas, ctx, assets, difficultyMap[gameDifficulty], getScore)
     }
-  }, [startGame, gameState, assets, gameDifficulty, getScore])
+  }, [startGame, gameState]) // , assets, gameDifficulty, getScore
 
   return <canvas ref={canvasRef} width={width} height={height} className='w-full' />
 }
